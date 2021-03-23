c10f6777f9632b4de1ef98cd3a4b7a8a3783c07d
Joha2001 <57204571+Joha2001@users.noreply.github.com>
2021-03-22 22:54:25 -0400

    Small Changes
    Testing out button and logo, changing some text.

--- src/App.js
+++ src/App.js
@@ -1,13 +1,20 @@
-import logo from './logo.svg';
+import logo from './logo.png';
 import './App.css';
 
 function App() {
+  function handleClick(e) {
+    e.preventDefault();
+    console.log('The link was clicked.');
+  }
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+        <button onClick={handleClick}><img src={logo} className="App-logo" alt="logo" /></button>
+        Click on the Steam Icon!
         <p>
-          Steam Project
+          CIS4301 Steam Project
+          <p></p>
+          Alex Good | John Hoang | Michael Klein | Jerry Mbamo
         </p>
       </header>
     </div>
