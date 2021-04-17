588841b0b3d5a856ec6dd41b23e5c3f15e3658d2
maklein20 <37639689+maklein20@users.noreply.github.com>
2021-04-17 09:54:52 -0400

    About/Documentation

--- src/components/About/About.js
+++ src/components/About/About.js
@@ -5,7 +5,19 @@ const About = () => {
         <header className="App-header">
         <div>
           <h1>CIS4301 Project Group 5</h1>
-          <p>Filler Text (Descriptions of Data, Links, Etc.)</p>
+          <h6>Created by: Alex Good, John Hoang, Jerry Mbamo, and Michael Klein</h6>
+          <p>------------------------------------------------------------</p>
+          <h5>Roles:</h5>
+          <h6>Frontend: Jerry Mbamo, John Hoang || Backend: Alex Good, Michael Klein</h6>
+          <p>-----------------------------------------------------------------------</p>
+          <p>This topic was selected because it was a topic we were all passionate about. Gaming is a unifying force</p>
+          <p>that gained popularity during 2020 when the COVID-19 pandemic hit. When everyone was stuck at home,</p>
+          <p>a vast majority of people had access to a computer with functional processing speed. With commute time</p>
+          <p>for many reduced, there was a lot of free time open that allowed many to try new things such as baking,</p>
+          <p>yoga, and gaming. Our app expands Steam usability from the primary focus being gamers to being accessible</p>
+          <p>to anyone who wants to get into gaming and gives a wider look on games they are interested on a graph</p>
+          <p>over time as opposed to simply the most recent reviews.</p>
+          <p>-----------------------------------------------------------------------</p>
        </div>
         </header>
       </div>
