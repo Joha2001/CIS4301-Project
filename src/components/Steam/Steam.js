9e94679fd5ad33d4f80c715fda73fab72001ac46
JerryMbamo <jmbamo@yahoo.com>
2021-04-17 20:50:13 -0400

    updated steam page
    added steam info and link to steam about

--- src/components/Steam/Steam.js
+++ src/components/Steam/Steam.js
@@ -1,18 +1,31 @@
-import React, {useState} from 'react';
+import React, { useState } from "react";
 
 const Steam = () => {
-  
-
-    return (
-      <div className="App">
-        <header className="App-header">
+  return (
+    <div className="App">
+      <header className="App-header">
         <div>
-          <h1>Filler</h1>
+          <h1>Steam </h1>
+          <p>
+            ------------------------------------------------------------------
+          </p>
+          <p>
+            Steam is a video game digital distribution service by Valve. It was
+            launched as a standalone software client in September 2003 as a way
+            for Valve to provide automatic updates for their games, and expanded
+            to include games from third-party publishers. Steam has also
+            expanded into an online web-based and mobile digital storefront.
+            Steam offers digital rights management (DRM), server hosting, video
+            streaming, and social networking services. It also provides the user
+            with installation and automatic updating of games, and community
+            features such as friends lists and groups, cloud storage, and
+            in-game voice and chat functionality.
+          </p>
         </div>
-        </header>
-      </div>
-       
-    );
-}
- 
+        <a href="https://store.steampowered.com/about/">More About Steam</a>
+      </header>
+    </div>
+  );
+};
+
 export default Steam;
